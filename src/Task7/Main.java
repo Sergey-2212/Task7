9c64bdfbe0ab47f88eb4f1c1995b5059085614aa
Sergey-2212 <svlakhmatov@gmail.com>
2022-04-18 08:59:55 +0300

    Ок

--- src/Task7/Main.java
+++ src/Task7/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         var bowl = new Bowl();
-        bowl.putFood(130);
+        bowl.putFood(150);
         Cat[] cats = Cat.initialize(7);
         for (Cat cat: cats
              ) { cat.isFullness();
